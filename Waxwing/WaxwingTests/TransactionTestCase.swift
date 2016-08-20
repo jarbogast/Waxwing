3a78b13dfcceca5b16f638191775ac67b487575c
Jonathan Arbogast <jonathan.arbogast@softwarebyassurance.com>
2016-08-19 20:49:01 -0400

    Reduced some duplication in test

--- Waxwing/WaxwingTests/TransactionTestCase.swift
+++ Waxwing/WaxwingTests/TransactionTestCase.swift
@@ -11,43 +11,34 @@ import XCTest
 
 class TransactionTestCase: XCTestCase {
     
-    func testTransactionWithNoEntries() {
-        let transactionLines = ["2015/10/12  Exxon"]
-        let transaction = Transaction(transactionLines: transactionLines)
-        
-        XCTAssertEqual(transaction.date, "2015/10/12")
-        XCTAssertEqual(transaction.payee, "Exxon")
-        XCTAssertNil(transaction.checkNumber)
-    }
-    
     func testTransactionWithTwoEntries() {
         let transactionLines = ["2015/10/12  Exxon",
                                 "   Expenses:Auto:Gas         $10.00",
                                 "   Liabilities:MasterCard   $-10.00"]
-        let transaction = Transaction(transactionLines: transactionLines)
         
         let expectedEntry1 = Entry(accountName: "Expenses:Auto:Gas", amount: 1000)
         let expectedEntry2 = Entry(accountName: "Liabilities:MasterCard", amount: -1000)
-        
-        XCTAssertEqual(transaction.date, "2015/10/12")
-        XCTAssertEqual(transaction.payee, "Exxon")
-        XCTAssertNil(transaction.checkNumber)
-        XCTAssertEqual(transaction.entries, [expectedEntry1, expectedEntry2])
+        let expectedEntries = [expectedEntry1, expectedEntry2]
+        assert(transactionLines: transactionLines, resultInTransactionWithDate: "2015/10/12", payee: "Exxon", checkNumber: nil, entries: expectedEntries)
     }
     
     func testTransactionWithCheckNumber() {
         let transactionLines = ["9/29 (1023) Pacific Bell",
                                 "Expenses:Utilities:Phone  $23.00",
                                 "Assets:Checking  $-23.00"]
-        let transaction = Transaction(transactionLines: transactionLines)
         
         let expectedEntry1 = Entry(accountName: "Expenses:Utilities:Phone", amount: 2300)
         let expectedEntry2 = Entry(accountName: "Assets:Checking", amount: -2300)
-        
-        XCTAssertEqual(transaction.date, "9/29")
-        XCTAssertEqual(transaction.payee, "Pacific Bell")
-        XCTAssertEqual(transaction.checkNumber, "1023")
-        XCTAssertEqual(transaction.entries, [expectedEntry1, expectedEntry2])
+        let expectedEntries = [expectedEntry1, expectedEntry2]
+        assert(transactionLines: transactionLines, resultInTransactionWithDate: "9/29", payee: "Pacific Bell", checkNumber: "1023", entries: expectedEntries)
     }
     
+    func assert(transactionLines: [String], resultInTransactionWithDate date: String, payee: String, checkNumber: String?, entries: [Entry]) {
+        let transaction = Transaction(transactionLines: transactionLines)
+        
+        XCTAssertEqual(transaction.date, date)
+        XCTAssertEqual(transaction.payee, payee)
+        XCTAssertEqual(transaction.checkNumber, checkNumber)
+        XCTAssertEqual(transaction.entries, entries)
+    }
 }
